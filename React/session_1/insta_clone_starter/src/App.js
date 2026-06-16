5129cd45c9f2333337d2a0fde2a4e6cb66a357ca
Karmur Aryan <aryankarmur@hotmail.com>
2026-06-16 11:19:36 +0530

    add session 1

---
+++ React/session_1/insta_clone_starter/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TrendingSong from "./TrendingSong";
+
+function App() {
+  return (
+    <>
+    {/* Task 1 is installation process */}
+    {/* Task 2 */}
+      <h1>Welcome to My React Zomato App</h1>
+      {/* Task 3 */}
+      <TrendingSong/>
+
+      {/* Task 4 */}
+      {/* 
+        virtual dom basically duplicate the dom and when the changes happendes in page it only replace that part of dom that was change.
+      */}
+    </>
+  );
+}
+
+export default App;
